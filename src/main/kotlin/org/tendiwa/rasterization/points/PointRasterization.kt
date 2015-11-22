fedfc85b99a99fa26c2113581669e8d5a57c1a7d
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-22 16:37:30 +0500

    Fix drawn object positioning

--- src/main/kotlin/org/tendiwa/rasterization/points/PointRasterization.kt
+++ src/main/kotlin/org/tendiwa/rasterization/points/PointRasterization.kt
@@ -2,8 +2,7 @@ package org.tendiwa.rasterization.points
 
 import org.tendiwa.geometry.points.Point
 import org.tendiwa.grid.tiles.Tile
-import org.tendiwa.math.doubles.closestInt
 
 val Point.tile: Tile
-    get() = Tile(x.closestInt, y.closestInt)
+    get() = Tile(x.toInt(), y.toInt())
 

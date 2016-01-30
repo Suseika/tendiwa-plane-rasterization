45f3eedc1ec902544add767f5b064fd4f36b5e16
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-30 20:39:44 +0500

    Change how Points rasterize to Tiles

--- src/main/kotlin/org/tendiwa/plane/rasterization/points/PointRasterization.kt
+++ src/main/kotlin/org/tendiwa/plane/rasterization/points/PointRasterization.kt
@@ -1,8 +1,9 @@
 package org.tendiwa.plane.rasterization.points
 
+import org.tendiwa.math.doubles.closestInt
 import org.tendiwa.plane.geometry.points.Point
 import org.tendiwa.plane.grid.tiles.Tile
 
 val Point.tile: Tile
-    get() = Tile(x.toInt(), y.toInt())
+    get() = Tile(x.closestInt, y.closestInt)
 

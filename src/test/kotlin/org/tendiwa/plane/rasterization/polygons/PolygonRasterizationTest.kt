9d99bd100c57674243502e88441b67b7f37cb3c5
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-31 08:03:55 +0500

    Add a test for rasterizing a polygon with an almost parallel edge

--- src/test/kotlin/org/tendiwa/plane/rasterization/polygons/PolygonRasterizationTest.kt
+++ src/test/kotlin/org/tendiwa/plane/rasterization/polygons/PolygonRasterizationTest.kt
@@ -1,6 +1,7 @@
 package org.tendiwa.plane.rasterization.polygons
 
 import org.junit.Test
+import org.tendiwa.plane.directions.CardinalDirection.*
 import org.tendiwa.plane.geometry.points.Point
 import org.tendiwa.plane.geometry.polygons.Polygon
 import org.tendiwa.plane.rasterization.polygon.rasterized
@@ -41,11 +42,28 @@ class PolygonRasterizationTest {
     }
 
     @Test
-    fun `rasterized a triangle`() {
+    fun `rasterizes a triangle`() {
         Polygon(
             Point(6.60156925537876, 2.19927888657355),
             Point(4.28376357283713, 3.48064706953204),
             Point(5.1318503223609, 5.01471066926644)
+        )
+            .rasterized
+            .apply { assert(tiles.size > 0) }
+    }
+
+    @Test
+    fun `rasterizes a polygon with an almost horizontal edge`() {
+        Polygon(
+            Point(0.0, 0.0),
+            {
+                move(10.0, E)
+                move(10.0 + 1e-11, N)
+                move(10.0, -2e-11)
+                move(10.0, E)
+                move(20.0, N)
+                move(30.0, W)
+            }
         )
             .rasterized
             .apply { assert(tiles.size > 0) }
